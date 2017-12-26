86a93f0c1dc7a42ec5745db1579fa402cbfc194d
devendervb12 <info@s-max.in>
2017-12-26 07:00:44 +0530

    changes

---
+++ WebContent/controller/DetailProduct.controller.js
@@ -0,0 +1,46 @@
+sap.ui.controller("smax.batch26.splitApp.controller.DetailProduct", {
+
+/**
+* Called when a controller is instantiated and its View controls (if available) are already created.
+* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+* @memberOf view.DetailProduct
+*/
+	onInit: function() {
+		
+		this.getOwnerComponent().getRouter(this).getRoute("detail").attachPatternMatched(function(oEvent){
+			
+			var productID = oEvent.getParameters().arguments.pid;
+			
+			this.getView().bindElement("/ProductSet('"+productID+"')");
+			
+		},this)
+
+	},
+
+/**
+* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+* (NOT before the first rendering! onInit() is used for that one!).
+* @memberOf view.DetailProduct
+*/
+//	onBeforeRendering: function() {
+//
+//	},
+
+/**
+* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+* This hook is the same one that SAPUI5 controls get after being rendered.
+* @memberOf view.DetailProduct
+*/
+//	onAfterRendering: function() {
+//
+//	},
+
+/**
+* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+* @memberOf view.DetailProduct
+*/
+//	onExit: function() {
+//
+//	}
+
+});
